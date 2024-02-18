dcccbe606886afe3c007f013bae24fc15bc7561c
Isak Steinmo Hansen <89584305+isakhansen00@users.noreply.github.com>
2024-02-18 14:09:03 +0100

    Added some code to the ads-b decoding to also check for potential jamming

--- PyModeS-Tests/pymodesfunctions.py
+++ PyModeS-Tests/pymodesfunctions.py
@@ -1,3 +1,3 @@
 import pyModeS as pms
-
-print(pms.decoder.adsb.nac_v("8d478891ea1f581e3d7c085a6cb3"))
+ads_b_message = ""
+print(pms.decoder.adsb.nac_p(ads_b_message))
--- PyModeS-Tests/testsubprocess.py
+++ PyModeS-Tests/testsubprocess.py
@@ -2,21 +2,22 @@ import subprocess
 import  pyModeS as mps
 
 def read_dump1090_raw():
-    # Run the dump1090 command and capture its output
+
     process = subprocess.Popen(['/home/admin/dump1090/./dump1090', '--raw'], stdout=subprocess.PIPE, universal_newlines=True)
     
-    # Read output line by line
     for line in process.stdout:
-        # Process each line as needed
-        hex_value = line.strip()  # Assuming each line contains one hexadecimal value
+        hex_value = line.strip()
         process_hex_value(hex_value)
 
 def process_hex_value(hex_value):
-    # Process each hexadecimal value here
     hex_value = hex_value.strip("*;")
     print("Received ADS-B signal:", hex_value)
     try:
-        print("NACp value: ", mps.decoder.adsb.nac_p(hex_value))
+        nac_p = mps.decoder.adsb.nac_p(hex_value)
+        flight_callsign = mps.adsb.callsign(hex_value)
+        print(f"Flight {flight_callsign} \nHas NACp value: {nac_p}")
+        if nac_p[0] < 4:
+            print(f"Flight {flight_callsign} Might be currently jammed \n NACp is: {nac_p[0]}")
     except RuntimeError:
         pass
 

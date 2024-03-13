ddf5b6b9293c94cb5b430b81669f2bb5dc11e412
patrikandreassen <pan029@post.uit.no>
2024-03-13 17:02:18 +0100

    Added print for printing hex values that comes in

--- Map/test.py
+++ Map/test.py
@@ -14,7 +14,7 @@ def read_dump1090_raw():
         hex_value = line.strip()
         hex_value = hex_value.replace("*", "")
         hex_value = hex_value.replace(";", "")
-        # print("Received ADS-B signal:", hex_value)
+        print("Received ADS-B signal:", hex_value)
         icao_address = mps.adsb.icao(hex_value)  # Extract ICAO address
         if icao_address is not None:
             hex_values_dict.setdefault(icao_address, []).append(hex_value)  # Accumulate hex values for the ICAO address

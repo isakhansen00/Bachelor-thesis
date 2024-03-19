f00f1cd93dee6b99422e6f012f5cb94b6f5318a2
patrikandreassen <pan029@post.uit.no>
2024-03-19 16:05:02 +0100

    Removed unused code

--- Map/test.py
+++ Map/test.py
@@ -14,7 +14,7 @@ def read_dump1090_raw():
         hex_value = line.strip()
         hex_value = hex_value.replace("*", "")
         hex_value = hex_value.replace(";", "")
-        print("Received ADS-B signal:", hex_value)
+        # print("Received ADS-B signal:", hex_value)
         icao_address = mps.adsb.icao(hex_value)  # Extract ICAO address
         if icao_address is not None:
             hex_values_dict.setdefault(icao_address, []).append(hex_value)  # Accumulate hex values for the ICAO address
@@ -27,7 +27,6 @@ def process_hex_values(icao_address):
     new_hex_values = hex_values[last_processed_index:]
     
     flight_callsign = None
-    nac_p = None
     msg_even = None
     msg_odd = None
     t_even = None
@@ -36,29 +35,20 @@ def process_hex_values(icao_address):
     for hex_value in new_hex_values:
         
         try:
-            nac_p = mps.decoder.adsb.nac_p(hex_value)
-            """
-            if nac_p[0] < 10:
-                print(f"Potential jamming detected. NACp is: {nac_p[0]}")
-                """
-        except RuntimeError:
-            pass
-        
-        try:
             flight_callsign = mps.adsb.callsign(hex_value)
         except RuntimeError:
             pass
 
         type_code_msg0 = mps.typecode(hex_value)
         
-        if type_code_msg0 in [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22]:
-            binary_msg = bin(int(hex_value, 16))[2:].zfill(112)  # Convert hex to binary
-            if binary_msg[55] == '0':
-                msg_even = hex_value
-                t_even = int(time.time())
-            elif binary_msg[55] == '1':
-                msg_odd = hex_value
-                t_odd = int(time.time())
+        # if type_code_msg0 in [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22]:
+        binary_msg = bin(int(hex_value, 16))[2:].zfill(112)  # Convert hex to binary
+        if binary_msg[55] == '0':
+            msg_even = hex_value
+            t_even = int(time.time())
+        elif binary_msg[55] == '1':
+            msg_odd = hex_value
+            t_odd = int(time.time())
                 
         if flight_callsign and msg_even and msg_odd and t_even and t_odd:
             try:
@@ -66,7 +56,6 @@ def process_hex_values(icao_address):
                 if position:
                     longitude, latitude = position
                     print(f"Flight {flight_callsign} with icao {icao_address} has position: LO: {longitude}, LA: {latitude}")
-                    # Save longitude and latitude to the database along with other information
                     msg_even = None
                     msg_odd = None
                     t_even = None

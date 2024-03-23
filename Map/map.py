3cd210531ac3517b547e808a28c9de9e68d65333
patrikandreassen <pan029@post.uit.no>
2024-03-23 11:27:27 +0100

    Added a popup that shows icao, longitude and latitude if you click on the airplane marker on the map

--- Map/map.py
+++ Map/map.py
@@ -5,5 +5,5 @@ def generate_map(flight_positions):
     for flight_id, points in flight_positions.items():
         folium.PolyLine(points[1:], color='black').add_to(map)
         last_point = points[-1]  # Get the last point of the flight path
-        folium.Marker(location=last_point, tooltip=points[0], icon=folium.Icon(color="blue", prefix="fa", icon="fa-plane")).add_to(map)
+        folium.Marker(location=last_point, tooltip=points[0], popup=f"ICAO: {flight_id}\n Longitude: {last_point[0]}\n Latitude: {last_point[1]}", icon=folium.Icon(color="blue", prefix="fa", icon="fa-plane")).add_to(map)
     map.save("map.html")

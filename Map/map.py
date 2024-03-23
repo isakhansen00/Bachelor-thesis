4dec543227d35f5a2d00e84edef562d5f3cdccfc
patrikandreassen <pan029@post.uit.no>
2024-03-23 11:48:38 +0100

    Added formating on the popup

--- Map/map.py
+++ Map/map.py
@@ -5,5 +5,8 @@ def generate_map(flight_positions):
     for flight_id, points in flight_positions.items():
         folium.PolyLine(points[1:], color='black').add_to(map)
         last_point = points[-1]  # Get the last point of the flight path
-        folium.Marker(location=last_point, tooltip=points[0], popup=f"ICAO: {flight_id}\n Longitude: {last_point[0]}\n Latitude: {last_point[1]}", icon=folium.Icon(color="blue", prefix="fa", icon="fa-plane")).add_to(map)
+        folium.Marker(location=last_point, 
+                      tooltip=points[0], 
+                      popup = 'ICAO: {}\nLongitude: {:.4f}\nLatitude: {:.4f}'.format(flight_id, last_point[0], last_point[1]), 
+                      icon=folium.Icon(color="blue", prefix="fa", icon="fa-plane")).add_to(map)
     map.save("map.html")

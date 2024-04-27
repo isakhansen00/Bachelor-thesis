6723ba2be829c73e6f4e4a63e0f10549f78d2f35
patrikandreassen <pan029@post.uit.no>
2024-04-27 07:39:30 +0200

    Added a fix

--- Map/teststatus.py
+++ Map/teststatus.py
@@ -1,11 +1,5 @@
 import asyncio
 from azure.iot.device.aio import IoTHubDeviceClient
-import time
-from datetime import datetime
-
-import asyncio
-from azure.iot.device.aio import IoTHubDeviceClient
-import time
 from datetime import datetime
 
 async def send_timestamp(device_client):
@@ -27,11 +21,7 @@ async def send_timestamp(device_client):
             await asyncio.sleep(10)  # Send timestamp every 10 seconds
 
 async def main():
-    pi_morkved = "HostName=RaspberryPiHubGruppe24.azure-devices.net;DeviceId=RaspberryPiMorkved;SharedAccessKey=xlXcAKxRMl5zAQRp58IQlSwPY9f1qFIovAIoTAT3hP0="
-    pi_fauske = "HostName=RaspberryPiHubGruppe24.azure-devices.net;DeviceId=RaspberryPiFauskeISE;SharedAccessKey=1q1iFmmcHsWgfhR7WaSKODew3PIHBjI/YAIoTDtYz1s="
-    pi_bodo = "HostName=RaspberryPiHubGruppe24.azure-devices.net;DeviceId=RaspberryPiBodo;SharedAccessKey=iZVIbvZjXqyrVhD18veLT363Fgp5luN5OAIoTPV3kls="
-
-    conn_str = pi_fauske
+    conn_str = "HostName=RaspberryPiHubGruppe24.azure-devices.net;DeviceId=RaspberryPiFauskeISE;SharedAccessKey=1q1iFmmcHsWgfhR7WaSKODew3PIHBjI/YAIoTDtYz1s="
     
     device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
     await device_client.connect()  # Connect to IoT Hub
@@ -50,44 +40,3 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
-"""
-async def update_device_twin(device_client):
-    last_reported_time = datetime.utcnow().isoformat() + 'Z'
-    twin_patch = {
-        "properties": {
-            "reported": {
-                "last_reported_time": last_reported_time
-            }
-        }
-    }
-    await device_client.patch_twin_reported_properties(twin_patch)
-
-async def send_heartbeat(device_client):
-    while True:
-        await update_device_twin(device_client)
-        print("Heartbeat sent")
-        await asyncio.sleep(10)  # Send heartbeat every 10 seconds
-
-async def main():
-    conn_str = "HostName=RaspberryPiHubGruppe24.azure-devices.net;DeviceId=RaspberryPiFauskeISE;SharedAccessKey=1q1iFmmcHsWgfhR7WaSKODew3PIHBjI/YAIoTDtYz1s="
-    
-    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
-    await device_client.connect()  # Connect to IoT Hub
-    
-    try:
-        # Start sending heartbeat
-        asyncio.create_task(send_heartbeat(device_client))
-        
-        while True:
-            await asyncio.sleep(1)
-    
-    except KeyboardInterrupt:
-        pass
-    finally:
-        await device_client.disconnect()  # Disconnect from IoT Hub
-
-if __name__ == '__main__':
-    asyncio.run(main())
-"""

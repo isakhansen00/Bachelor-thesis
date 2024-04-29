1832132dcd0fa38cb99d2002726fce3903da5a2a
patrikandreassen <pan029@post.uit.no>
2024-04-29 14:52:36 +0200

    Cleaned the code and removed unused code

--- UI/status_test.py
+++ UI/status_test.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, render_template
-import asyncio
-from datetime import datetime, timedelta
 from azure.iot.hub import IoTHubRegistryManager
-import json
+import asyncio
+
+# Create a Flask app instance
 app = Flask(__name__)
 
 # Dictionary to hold device information
@@ -21,39 +21,37 @@ devices = {
 # Connection string for the IoT Hub service shared access policy
 CONNECTION_STRING = "HostName=RaspberryPiHubGruppe24.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=5R/rIBxvhyOJVoJ+rg2xsklVz8GrZAvK0AIoTHUDvKQ="
 
+# Asynchronous function to get the status of a device
 async def get_device_status(device_id):
     try:
         # Create an instance of the IoTHubRegistryManager
         registry_manager = IoTHubRegistryManager.from_connection_string(CONNECTION_STRING)
         
-        # Get the device twin
+        # Get the device twin and convert it to a dictionary
         twin = (registry_manager.get_twin(device_id)).as_dict()
-        #twin = jsonify(twin)
-        print((twin['connection_state']))
-        #print(twin)
-        # Extract last reported time from twin
-        #last_reported_time_str = twin['reported']['properties']['reported']['last_reported_time' + device_id]
-
-        print(twin['connection_state'])
-        # Check if the device hasn't reported for more than 15 seconds
+        
+        # Extract connection state from twin
         return twin['connection_state']
     
     except Exception as e:
-        return f"Disconnected"
+        # If an exception occurs, return "Disconnected" status
+        return "Disconnected"
 
+# Route to retrieve the status of all devices
 @app.route('/status')
 async def status():
     sensor_status = {}  # Dictionary to hold device statuses
-    tasks = []  # List to hold tasks
+
+    # Create a list of asynchronous tasks to retrieve the status of each device
+    tasks = [get_device_status(device_id) for device_id in devices.keys()]
     
-    for device_id, _ in devices.items():
-        tasks.append(get_device_status(device_id))
-    
+    # Gather the results of asynchronous tasks
     results = await asyncio.gather(*tasks)
     
-    for device_id, status in zip(devices.keys(), results):
-        sensor_status[device_id] = status
+    # Update the sensor_status dictionary with device statuses
+    sensor_status.update(zip(devices.keys(), results))
     
+    # Return the device statuses as JSON
     return jsonify(sensor_status)
 
 @app.route('/')
@@ -62,3 +60,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+

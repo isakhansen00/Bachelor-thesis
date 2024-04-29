248915ac4c1c0a8544724a3499a92d657c8c61a8
patrikandreassen <pan029@post.uit.no>
2024-04-29 17:25:11 +0200

    Added new conncetion string

--- ADSB-Decoding/decodeADSB.py
+++ ADSB-Decoding/decodeADSB.py
@@ -11,12 +11,10 @@ import asyncio
 
 hex_values_dict = {}
 
-CONNECTION_STRING = "HostName=RaspberryPiSDRHub.azure-devices.net;DeviceId=RaspberryPi;SharedAccessKey=Z3FE1PNea9Oz/xo8ofj4vMRpMDlwJCUmJAIoTN1a+QY="
-MSG_SND = ''
-client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-
 conn_str = str(os.getenv("CONNECTION_STRING"))
 device_id = str(os.getenv("DEVICE_ID"))
+MSG_SND = ''
+client = IoTHubDeviceClient.create_from_connection_string(conn_str)
  
 def read_dump1090_raw():
     process = subprocess.Popen(['/home/admin/dump1090/./dump1090', '--raw', '--net'], stdout=subprocess.PIPE, universal_newlines=True)
@@ -76,7 +74,7 @@ if __name__ == "__main__":
     dump_thread = threading.Thread(target=read_dump1090_raw)
     dump_thread.start()
     #dump_thread.join()
-    #asyncio.run(send_sensor_status(conn_str, device_id))
+    asyncio.run(send_sensor_status(conn_str, device_id))
 
 
 

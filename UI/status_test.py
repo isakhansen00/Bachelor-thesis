d98f9a68d0552722441c6923472ee8a4f6056d43
Isak Steinmo Hansen <89584305+isakhansen00@users.noreply.github.com>
2024-04-29 10:49:13 +0200

    Fixed twin functionality to report if device is connected or disconnected

--- UI/status_test.py
+++ UI/status_test.py
@@ -1,67 +1,59 @@
-from azure.iot.device.aio import IoTHubDeviceClient
 from flask import Flask, jsonify, render_template
 import asyncio
 from datetime import datetime, timedelta
-
+from azure.iot.hub import IoTHubRegistryManager
+import json
 app = Flask(__name__)
 
 # Dictionary to hold device information
 devices = {
     "RaspberryPiMorkved": {
-        "connection_string": "HostName=RaspberryPiHubGruppe24.azure-devices.net;DeviceId=RaspberryPiMorkved;SharedAccessKey=xlXcAKxRMl5zAQRp58IQlSwPY9f1qFIovAIoTAT3hP0="
+        "device_id": "RaspberryPiMorkved"
     },
     "RaspberryPiFauskeISE": {
-        "connection_string": "HostName=RaspberryPiHubGruppe24.azure-devices.net;DeviceId=RaspberryPiFauskeISE;SharedAccessKey=1q1iFmmcHsWgfhR7WaSKODew3PIHBjI/YAIoTDtYz1s="
+        "device_id": "RaspberryPiFauskeISE"
     },
     "RaspberryPiBodo": {
-        "connection_string": "HostName=RaspberryPiHubGruppe24.azure-devices.net;DeviceId=RaspberryPiBodo;SharedAccessKey=iZVIbvZjXqyrVhD18veLT363Fgp5luN5OAIoTPV3kls="
+        "device_id": "RaspberryPiBodo"
     }
 }
 
-"""
-Function to get the status of a device (online or offline)
-"""
-async def get_device_status(device_client, device_id):
+# Connection string for the IoT Hub service shared access policy
+CONNECTION_STRING = "HostName=RaspberryPiHubGruppe24.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=5R/rIBxvhyOJVoJ+rg2xsklVz8GrZAvK0AIoTHUDvKQ="
+
+async def get_device_status(device_id):
     try:
-        # Get device twin
-        twin = await device_client.get_twin()
+        # Create an instance of the IoTHubRegistryManager
+        registry_manager = IoTHubRegistryManager.from_connection_string(CONNECTION_STRING)
+        
+        # Get the device twin
+        twin = (registry_manager.get_twin(device_id)).as_dict()
+        #twin = jsonify(twin)
+        print((twin['connection_state']))
+        #print(twin)
         # Extract last reported time from twin
-        last_reported_time_str = twin['reported']['properties']['reported']['last_reported_time_' + device_id]
-        last_reported_time = datetime.strptime(last_reported_time_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+        #last_reported_time_str = twin['reported']['properties']['reported']['last_reported_time' + device_id]
+
+        print(twin['connection_state'])
         # Check if the device hasn't reported for more than 15 seconds
-        if datetime.utcnow() - last_reported_time > timedelta(seconds=15):
-            return "Offline"
-        return "Online"
-    # If an exception occurs, consider device as offline
+        return twin['connection_state']
+    
     except Exception as e:
-        return "Offline"
+        return f"Disconnected"
 
 @app.route('/status')
 async def status():
-
     sensor_status = {}  # Dictionary to hold device statuses
-    device_clients = []  # List to hold device clients
     tasks = []  # List to hold tasks
     
-    # Loop through each device and create a client for it
-    for device_id, device_info in devices.items():
-        device_client = IoTHubDeviceClient.create_from_connection_string(device_info["connection_string"])
-        device_clients.append(device_client)
-        await device_client.connect()
-        tasks.append(get_device_status(device_client, device_id))
+    for device_id, _ in devices.items():
+        tasks.append(get_device_status(device_id))
     
-    # Wait for all tasks to complete
     results = await asyncio.gather(*tasks)
     
-    # Populate sensor_status dictionary with device statuses
     for device_id, status in zip(devices.keys(), results):
         sensor_status[device_id] = status
     
-    # Disconnect all device clients
-    for device_client in device_clients:
-         await device_client.disconnect()
-    
-    # Return device statuses as JSON
     return jsonify(sensor_status)
 
 @app.route('/')
